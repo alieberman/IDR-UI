10f1e9fb13aa6cf0e02af7459ac0a755abe611ec
Aaron Lieberman <aaron@bigcompass.com>
2015-01-22 10:32:27 -0700

    bower

--- app/scripts/controllers/AccountShowCtrl.js
+++ app/scripts/controllers/AccountShowCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('idrApp')
-  .controller('AccountShowCtrl', function ($scope, $resource, $location, $routeParams, Feed) {
+  .controller('AccountShowCtrl', function ($scope, $resource, $location, $routeParams, Accounts) {
 	
   });
